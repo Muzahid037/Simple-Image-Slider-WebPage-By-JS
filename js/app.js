d34b8bcce162f64761ee855f78f7b476a218a74c
Md. Muzahidul Islam <48602232+Muzahid037@users.noreply.github.com>
2021-09-17 01:35:17 +0600

    Conditional (ternary) operator removed with modulo operator

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
+let images = [], x = -1;
+images[0] = "image/0.jpg";
+images[1] = "image/1.jpg";
+images[2] = "image/2.jpg";
+images[3] = "image/3.jpg";
+images[4] = "image/4.jpg";
+
 function displayNextImage() {
-    x = (x === images.length - 1) ? 0 : x + 1;
+    //x = (x === images.length - 1) ? 0 : x + 1;
+    x++;
+    x %= images.length;
     document.getElementById("img").src = images[x];
 }
 
 function displayPreviousImage() {
-    x = (x <= 0) ? images.length - 1 : x - 1;
+    //x = (x <= 0) ? images.length - 1 : x - 1;
+    x--;
+    x %= images.length;
     document.getElementById("img").src = images[x];
 }
 
 function startTimer() {
     setInterval(displayNextImage, 3000);
 }
-
-var images = [], x = -1;
-images[0] = "image/0.jpg";
-images[1] = "image/1.jpg";
-images[2] = "image/2.jpg";
-images[3] = "image/3.jpg";
-images[4] = "image/4.jpg";
 
 
 //startTimer();
